13023edd47c8b91c3f5d390c4a58d1fd0cc74c06
DanieII <danielbogdanov1748@gmail.com>
2023-10-27 22:51:07 +0300

    Added a way to choose a profile type

--- PaymentService/authentication/urls.py
+++ PaymentService/authentication/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
-from .views import UserLoginView, UserRegisterView
+from .views import (
+    UserLoginView,
+    UserLogoutView,
+    UserRegisterView,
+    BusinessUserRegisterView,
+    CustomerUserRegisterView,
+)
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path(
+        "register/business",
+        BusinessUserRegisterView.as_view(),
+        name="register_business",
+    ),
+    path(
+        "register/customer",
+        CustomerUserRegisterView.as_view(),
+        name="register_customer",
+    ),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
 ]
