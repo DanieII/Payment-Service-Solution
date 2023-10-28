a5ae798fb69b079390c1d96cf08493bd50178f18
chebishev <atanas.chebishev@gmail.com>
2023-10-28 23:19:47 +0300

    added coinbase and stripe payment options

--- PaymentService/products/urls.py
+++ PaymentService/products/urls.py
@@ -5,6 +5,9 @@ from .views import (
     ListingsView,
     product_list,
     product_details,
+    SuccessView, CancelView,
+    CreateStripeCheckoutSessionView,
+    create_coinbase_checkout_session_view,
 )
 
 urlpatterns = [
@@ -13,4 +16,9 @@ urlpatterns = [
     path("listings/", ListingsView.as_view(), name="listings"),
     path("listings/<int:pk>/", ListingDetails.as_view(), name="listing-details"),
     path("listings/add/", AddListing.as_view(), name="add-listing"),
+    path("create-checkout-session/<int:pk>/", CreateStripeCheckoutSessionView.as_view(),
+         name="create-checkout-session"),
+    path("success/", SuccessView.as_view(), name="success"),
+    path("cancel/", CancelView.as_view(), name="cancel"),
+    path('crypto-session/<int:id>/', create_coinbase_checkout_session_view, name='crypto-session'),
 ]
